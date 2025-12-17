cc04b3bdd2b5d0337cfca328a41f3024da2b9ce4
SludgyEar <l21140258@queretaro.tecnm.mx>
2025-12-17 11:28:12 -0600

    Agregando proyecto

---
+++ src/main/java/rest/pkbe/PkbeApplication.java
@@ -0,0 +1,13 @@
+package rest.pkbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkbeApplication.class, args);
+	}
+
+}
